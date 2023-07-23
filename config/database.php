183dd03ccbaaf53edf14dc1439fd4e7965533a8e
mreynosof <mreynosof@unjbg.edu.pe>
2023-07-23 18:07:13 -0500

    xd

--- config/database.php
+++ config/database.php
@@ -57,12 +57,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
-
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem',
-            ]) : [],
-            
+            'engine' => null,           
         ],
 
         'pgsql' => [
